d8e6ba6674a87b372094e247c6f591dd95b42c49
Raian Tasfin <raian.tasfin-prog@yandex.com>
2026-05-06 22:46:09 +0600

    parsing upto print, assignment, expressions

--- testcases/full.ml
+++ testcases/full.ml
@@ -1,7 +1,12 @@
 var a := 1
 var b := 2
-var c := 3
+var c := 3 :> line comment here var c := 3
 
 var d := (a * b) - c
 
+:> line comment here var c := 3
+
+
 print(d)
+
+:> line comment here var c := 3
